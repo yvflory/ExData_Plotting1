cf2310cce3651b461254c039bdda3796b2975abb
Yvonne Flory <yvflory@gmail.com>
2015-02-06 17:03:03 +0000

    plot 1

--- plot1.R
+++ plot1.R
@@ -1,5 +1,18 @@
-makeGraph <- function() {
-    ## Didn't have time to go beyond this, unfortunately.
-    data <- fread("household_power_consumption.txt", colClasses = c("Date" = "character", "Time" = "character"))
+plot1 <- function() {
+    ## open screen device, make file
+    png("plot1.png")
+    
+    ## read in data
+    ## had a little problem with the subsetting, so I'm using a manually
+    ## subsetted file. Didn't have time to fix that.
+    data <- fread("household_power_consumption_subset.txt")
+    
+    ## Make the plot, customised colour, title and x-axis text.
+    new_hist <- hist(data[["Global_active_power"]], 
+                     main = "Global active power", 
+                     xlab = "Global active power (kilowatts)", 
+                     col = "red")
 
+    ## close screen device
+    dev.off()
 }
