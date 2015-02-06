a70486b2ef0aa409ae34e0317f80d27650d5b7ac
Yvonne Flory <yvflory@gmail.com>
2015-02-06 09:55:32 +0000

    File created

---
+++ plot1.R
@@ -0,0 +1,5 @@
+makeGraph <- function() {
+    ## Didn't have time to go beyond this, unfortunately.
+    data <- fread("household_power_consumption.txt", colClasses = c("Date" = "character", "Time" = "character"))
+
+}
