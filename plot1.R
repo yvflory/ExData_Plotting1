347c3adc869c00cf21ed525503ad12802b6a23d9
Yvonne Flory <yvflory@gmail.com>
2015-02-06 17:36:40 +0000

    added subsetting

--- plot1.R
+++ plot1.R
@@ -3,9 +3,11 @@ plot1 <- function() {
     png("plot1.png")
     
     ## read in data
-    ## had a little problem with the subsetting, so I'm using a manually
-    ## subsetted file. Didn't have time to fix that.
-    data <- fread("household_power_consumption_subset.txt")
+    require(sqldf)
+    file <- c("household_power_consumption.txt")
+    data <- read.csv.sql(file, header = T, sep=";", 
+                                sql = "select * from file where (Date == '1/2/2007' OR Date == '2/2/2007')" )
+    #data <- fread("household_power_consumption_subset.txt")
     
     ## Make the plot, customised colour, title and x-axis text.
     new_hist <- hist(data[["Global_active_power"]], 
